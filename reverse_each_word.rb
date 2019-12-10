eba166506d1fc0ce6db3fb6badd6722ac58fe2a5
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 20:32:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(string) 
   array = string.split(" ")
   new_array = []
-  i = 0 
 
   
   new_array = array.collect{|memo| memo.reverse}
 # The above line comprises the following 4 lines into 1 line: 
+  # i = 0 
   # while i < array.length 
   #   new_array[i] = array[i].reverse
   #   i+=1 
